a8993aecba22ad3fe7208354e657dfc7e7b522df
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-16 19:02:46 -0300

    Adiciona rota de cadastro

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Login from './views/login/login.js';
+import Cadastro from './views/cadastro/cadastro.js';
 import Router from './util/Router.js';
 
 function routing() {
   const router = new Router({
     login: new Login(),
+    cadastro: new Cadastro(),
   });
 
   router.handleHashChange();
---
+++ src/views/cadastro/cadastro.js
@@ -0,0 +1,29 @@
+import Form from '../../components/Form/Form.js';
+import { URL_CADASTRO } from '../../util/routes.js';
+import { redirect } from '../../util/Router.js';
+
+export default class Login extends Form {
+  constructor() {
+    const title = 'Cadastro';
+    const actionName = 'Cadastrar';
+    const formFields = [
+      { name: 'Email', type: 'email' },
+      { name: 'Senha', type: 'password' },
+      { name: 'Primeiro nome', type: 'text' },
+      { name: 'Ultimo nome', type: 'text' },
+      { name: 'Numero do cartao', type: 'text' },
+    ];
+    const handleWorking = () => {
+      alert('Usuario criado com sucesso!');
+      redirect('login');
+    };
+
+    super(title, actionName, formFields, handleWorking, URL_CADASTRO);
+  }
+
+  renderComponent() {
+    return '<cadastro-form></cadastro-form>';
+  }
+}
+
+customElements.define('cadastro-form', Login);
