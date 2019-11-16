98e47474a3d85c2efb6630f3a96dfbeaa83f67ca
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-16 18:00:28 -0300

    Adiciona classe de Router

---
+++ src/util/Router.js
@@ -0,0 +1,31 @@
+export function redirect(locationTo) {
+  location.hash = locationTo.toLowerCase();
+}
+
+export default class Router {
+  constructor(routes) {
+    this.routes = routes;
+  }
+
+  handleHashChange() {
+    const app = document.querySelector('#app');
+
+    if (location.hash !== location.hash.toLowerCase()) {
+      redirect(location.hash);
+    } else {
+      const hash = this.treatHash(location.hash);
+      const route = hash in this.routes ? this.routes[hash] : this.notFound();
+      app.innerHTML = route.renderComponent();
+    }
+  }
+
+  treatHash(hash) {
+    return hash === '' ? 'root' : hash.substring(1);
+  }
+
+  notFound() {
+    return {
+      renderComponent: () => '<h1>Not Found</h1>',
+    };
+  }
+}
