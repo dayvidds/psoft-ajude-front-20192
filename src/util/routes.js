d3b07e6fe43ca8b61d5b7ef754c37fd44d1b0495
dayvidds <dayvid.silva@ccc.ufcg.edu.br>
2019-11-19 00:06:02 -0300

    Altera rota de cadastro do usuario

--- src/util/routes.js
+++ src/util/routes.js
@@ -2,7 +2,7 @@ export const API_URI = 'http://localhost:8080';
 
 const API_URL = `${API_URI}/api/v1`;
 
-export const URL_CADASTRO = `${API_URL}/cadastro`;
+export const URL_CADASTRO = `${API_URL}/cadastro/usuario`;
 export const URL_LOGIN = `${API_URL}/autorizacao/login`;
 
 export default API_URL;
