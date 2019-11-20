9d794795ca5e7091956aec5ec9b2293f60e5499b
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-20 16:30:51 -0300

    Adiciona rota de campanha da API

--- src/util/api-routes.js
+++ src/util/api-routes.js
@@ -3,6 +3,7 @@ export const API_URI = 'http://localhost:8080';
 const API_URL = [API_URI, 'api', 'v1'].join('/');
 
 export const API_URL_USUARIO = [API_URL, 'usuario'].join('/');
+export const API_URL_CAMPANHA = [API_URL, 'campanha'].join('/');
 export const API_URL_LOGIN = [API_URL, 'autorizacao', 'login'].join('/');
 
 export default API_URL;
