6c3fb5ee6a4b7a77781089bec4d07bb4dd232c9e
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-24 11:12:33 -0300

    Forca autenticacao no cadastro de campanha

--- src/views/cadastro-campanha/CadastroCampanha.js
+++ src/views/cadastro-campanha/CadastroCampanha.js
@@ -1,9 +1,15 @@
 import BaseView from '../base-view/BaseView.js';
 import CadastroCampanhaForm from './CadastroCampanhaForm.js';
+import { forceAuth } from '../../util/auth.js';
 
 const componentName = 'cadastro-campanha';
 
 export default class CadastroCampanha extends BaseView {
+  constructor() {
+    super();
+    forceAuth();
+  }
+
   render() {
     return `${CadastroCampanhaForm.renderComponent()}`;
   }
