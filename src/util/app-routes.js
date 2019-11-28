4e13276bdda3bfaff9214d25862c173e4b2a59a9
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-27 23:35:25 -0300

    Adiciona novas rotas para a aplicação

--- src/util/app-routes.js
+++ src/util/app-routes.js
@@ -3,6 +3,8 @@ export const URL_ROOT = '/';
 export const URL_LOGIN = [URL_ROOT, 'login'].join('');
 export const URL_CADASTRO = [URL_ROOT, 'cadastro'].join('');
 export const URL_CAMPANHA = [URL_ROOT, 'campanha'].join('');
+export const URL_CADASTRO_CAMPANHA = [URL_ROOT, 'cadastro_campanha'].join('');
+export const URL_USUARIO = [URL_ROOT, 'usuario'].join('');
 export const URL_PESQUISA = [URL_ROOT, 'pesquisa'].join('');
 
 export const URL_PESQUISA_CAMPANHA = [URL_PESQUISA, 'campanha'].join('/');
