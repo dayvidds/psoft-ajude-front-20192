0e7111892227e2ff45ef53205c02030d385acb99
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-27 23:39:13 -0300

    Adiciona novos metodos de request

--- src/util/mocked-requests.js
+++ src/util/mocked-requests.js
@@ -4,6 +4,15 @@ export function getRequest(url) {
   return fetch(url);
 }
 
+export function getRequestWithToken(url) {
+  return fetch(url, {
+    method: 'GET',
+    headers: new Headers({
+      Authorization: `Bearer ${getToken()}`,
+    }),
+  });
+}
+
 export function postRequest(url, data) {
   return fetch(url, {
     method: 'POST',
@@ -17,6 +26,17 @@ export function postRequest(url, data) {
 export function postRequestWithToken(url, data) {
   return fetch(url, {
     method: 'POST',
+    body: JSON.stringify(data),
+    headers: new Headers({
+      Authorization: `Bearer ${getToken()}`,
+      'Content-Type': 'application/json',
+    }),
+  });
+}
+
+export function deleteRequestWithToken(url, data) {
+  return fetch(url, {
+    method: 'DELETE',
     body: JSON.stringify(data),
     headers: new Headers({
       Authorization: `Bearer ${getToken()}`,
