8c2e37e9f10dbf22497801f31c76b786be001423
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-27 23:40:11 -0300

    Adiciona novos metodos util

--- src/util/util.js
+++ src/util/util.js
@@ -20,3 +20,30 @@ export function getUrlParameter(name) {
   const results = regex.exec(url);
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
+
+export function formatDate(date) {
+  return date
+    .split('T')[0]
+    .split('-')
+    .reverse()
+    .join('/');
+}
+
+export function formataDinheiro(quantia) {
+  return `R$${quantia.toFixed(2)}`;
+}
+export function renderListFromFunction(name, list, func, content) {
+  return `
+    <div class="card">
+      <h3>${capitalize(name)}</h3>
+      ${content === undefined ? '' : content}
+      <ul class="${name}">
+        ${list.length === 0 ? `Sem ${name}` : list.map(func).join('')}
+      </ul>
+    </div>
+  `;
+}
+
+export function getDinamicUrlValue(index) {
+  return location.hash.split('/')[index];
+}
