90427437f911dda4fe7fe4c6988ce5a54762a2e7
Pedro Espindula <joaopedro.jpse@gmail.com>
2019-11-27 23:36:15 -0300

    Adiciona suporte a email ao auth.js

--- src/util/auth.js
+++ src/util/auth.js
@@ -2,9 +2,14 @@ import { redirect } from './router/Router.js';
 import { URL_LOGIN, URL_ROOT } from './app-routes.js';
 
 const TOKEN_KEY = 'token';
+const EMAIL_KEY = 'email';
 
 export function getToken() {
   return localStorage.getItem(TOKEN_KEY);
+}
+
+export function getEmail() {
+  return localStorage.getItem(EMAIL_KEY);
 }
 
 export function isLoggedIn() {
@@ -23,7 +28,8 @@ export function forceAuth() {
   }
 }
 
-export function login(token) {
+export function login(token, email) {
+  localStorage.setItem(EMAIL_KEY, email);
   localStorage.setItem(TOKEN_KEY, token);
   redirect(URL_ROOT);
 }
